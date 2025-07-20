554aa45fe46e366fe1bc622dee09c3ddca8b7507
harrisonvanderbyl <harrison.vanderbyl@gmail.com>
2025-07-20 01:52:34 +0000

    all things

--- build.sh
+++ build.sh
@@ -1,5 +1,21 @@
 mkdir ./build/
 # cpp -nostdinc -undef -x assembler-with-cpp -E ./vivo.dts -I./files > ./build/vivo.dts
 # dtc -I dts -O dtb ./build/vivo.dts -o ./build/vivo.dtb
-cpp -nostdinc -undef -x assembler-with-cpp -E ./surface.dts -I./files > ./build/surface.dts
-dtc -I dts -O dtb ./build/surface.dts -o ./build/surface.dtb
+rm ./build/surface.dts
+cpp -nostdinc -undef -x assembler-with-cpp -E ./sp12.dts -I./dts -I./dt_includes -I./include > ./build/surface.dts
+rm ./build/x1e80100-microsoft-denali.dtb
+dtc -I dts -O dtb ./build/surface.dts -o ./build/x1e80100-microsoft-denali.dtb
+# if no output, then the dtc command failed
+if [ ! -f ./build/x1e80100-microsoft-denali.dtb ];
+then
+    echo "Error: dtc command failed, no output file created."
+    exit 1
+fi
+sudo cp ./build/x1e80100-microsoft-denali.dtb /boot/dtbs/6.16.0-rc5-jg-1-qcom-x1e/qcom/x1p42100-lenovo-ideapad-5-2in1.dtb
+sudo rm /var/log/journal/fa46730e33564e71aa0bc4196801cd6f/*
+# rm ./build/surface.dts
+# flush usb drive
+sync
+# unmount usb drive
+# sudo umount /run/media/harrison/root2410
+# echo "Build complete. Output file
